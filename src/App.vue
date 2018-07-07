9e90f18c97fa7398282846bb67ba8f106204faea
zzl20180410 <512890686@qq.com>
2018-07-08 04:13:49 +0800

    修改弹窗

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,21 @@
     <img src="./assets/logo.png">
     <router-view/>
     <el-button>默认按钮</el-button>
-    <el-button type="primary">主要按钮</el-button>
+    <el-button type="primary" @click="show">主要按钮</el-button>
     <el-button type="text">文字按钮</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+
+  methods:{
+  show(){
+    alert(123);
+  }
+}
+
 }
 </script>
 
