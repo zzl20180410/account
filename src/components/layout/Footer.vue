8fe578384d294c2a7ce7aecde016a2e001bf67ba
zzl20180410 <512890686@qq.com>
2018-08-03 23:34:13 +0800

    添加主页面信息

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="footer">
+    <p>版权所有 @zzl 2018-08-03 保留一切权利 意见反馈</p>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    p{
+        font-size: 12px;
+        margin: 5px 0 0 0;
+    }
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <p>欢迎你的到来</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
