73a934e7a61e866815a0aa83f1dd48e9eca659ae
zzl20180410 <512890686@qq.com>
2018-08-03 23:35:35 +0800

    修改主页面信息

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,65 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-    <el-button>默认按钮</el-button>
-    <el-button type="primary" @click="show">主要按钮</el-button>
-    <el-button type="text">文字按钮</el-button>
+  <div id="app">  
+    <el-container>
+      <el-header class="account-header"><Header></Header></el-header>
+
+      <el-container class="account-conter">
+        <el-aside class="conter-aside"><Menu></Menu></el-aside>
+        <el-main class="conter-main"><router-view></router-view></el-main>
+      </el-container>
+
+      <el-footer class="account-footer"><Footer></Footer></el-footer>
+    </el-container>
   </div>
+
+
+
 </template>
 
 <script>
+import Header from './components/layout/Header.vue'
+import Footer from './components/layout/Footer.vue'
+import Menu from './components/layout/Menu.vue'
+
 export default {
   name: 'App',
 
   methods:{
-  show(){
+    show(){
     alert(123);
+    }
+  },
+  components:{
+    Header,Footer,Menu
   }
-}
 
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .account-header{
+        height: 50px;
+        z-index: 9;
+        background-color: aqua;
+    }
+    /* .account-conter{
+        
+    } */
+    .conter-aside{
+        width: 230px;
+        /* height: 100%; */
+        height: auto;
+        background-color: bisque;
+
+    }
+    .conter-main{
+        height: 100%;
+        background-color: brown;
+    }
+    .account-footer{
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        background-color: chartreuse;
+    }
 </style>
