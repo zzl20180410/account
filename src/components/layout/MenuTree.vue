2c334a783d8c709ca187ed733c57268b7e1887e8
zzl20180410 <512890686@qq.com>
2018-08-12 04:48:01 +0800

    新增菜单配置

---
+++ src/components/layout/MenuTree.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <template v-for="value in this.menuData">
+        <el-submenu :index="value.index"  v-if="value.node">
+            <template slot="title">
+                <i :class="value.icon"></i>
+                <span slot="title">{{value.menu_name}}</span>
+            </template>
+            <MenuTree :menuData="value.node"></MenuTree>
+        </el-submenu>
+        <el-menu-item  :index="value.index" v-else>
+            <i :class="value.icon"></i>
+            <span slot="title">{{value.menu_name}}</span>
+        </el-menu-item>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["menuData"],
+  name: "MenuTree"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
